3e038363e109df63769ae6c91fb29399a652061c
Venkat <venkatdevops2009@gmail.com>
2026-06-13 16:36:34 +0530

    first commit

--- args.sh
+++ args.sh
@@ -1,10 +1,9 @@
-echo "Please enter your username::"
-read username
-echo "Please enter your country::"
-read country
-echo "Hello $username from $country!"
-echo "number of arguments passed: $#"
-echo "first argument: $1"
-echo "second argument: $2"
-echo "All arguments: $@"
-echo "script name: $0"
+#!/bin/bash
+
+echo "User $1 is from $2"
+
+echo "Total number of arguments passed: $#"
+
+echo "All arguments passed: $@"
+
+echo "Script name: $0"
